6169f672244992f0dfaa792f800a62b78f41db22
Assyl <to1egeno8naa@gmail.com>
2023-10-22 16:57:35 +0600

    Добавьте файлы проекта.

---
+++ Day7(Tanks)/Program.cs
@@ -0,0 +1,41 @@
+﻿using MyLibrary.WordOfTanks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7_Tanks_
+{
+    
+        class Program
+        {
+            static void Main()
+            {
+                Tank[] t34Tanks = new Tank[5];
+                Tank[] panteraTanks = new Tank[5];
+
+
+                for (int i = 0; i < 5; i++)
+                {
+                    t34Tanks[i] = new Tank("Т-34");
+                    panteraTanks[i] = new Tank("Pantera");
+                }
+
+                for (int i = 0; i < 5; i++)
+                {
+                    bool t34Wins = t34Tanks[i] ^ panteraTanks[i];
+
+                    if (t34Wins)
+                    {
+                        Console.WriteLine($"Бой {i + 1}: Т-34 победил Pantera");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Бой {i + 1}: Pantera победил Т-34");
+                    }
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
---
+++ MyLibrary/WordOfTanks/Tank.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary.WordOfTanks
+{
+        public class Tank
+        {
+            public string Name { get; private set; }
+            public int AmmunitionLevel { get; private set; }
+            public int ArmorLevel { get; private set; }
+            public int ManeuverabilityLevel { get; private set; }
+
+            public Tank(string name)
+            {
+                Name = name;
+                Random random = new Random();
+                AmmunitionLevel = random.Next(0, 101);
+                ArmorLevel = random.Next(0, 101);
+                ManeuverabilityLevel = random.Next(0, 101);
+            }
+
+            public override string ToString()
+            {
+                return $"{Name}: Ammo={AmmunitionLevel}, Armor={ArmorLevel}, Maneuverability={ManeuverabilityLevel}";
+            }
+
+            public static bool operator ^(Tank tank1, Tank tank2)
+            {
+                int advantageCount = 0;
+
+                if (tank1.AmmunitionLevel > tank2.AmmunitionLevel)
+                    advantageCount++;
+                if (tank1.ArmorLevel > tank2.ArmorLevel)
+                    advantageCount++;
+                if (tank1.ManeuverabilityLevel > tank2.ManeuverabilityLevel)
+                    advantageCount++;
+
+                return advantageCount >= 2;
+            }
+        }
+    }
+
+
